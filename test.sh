4093dfd732a014b7fa6d5f0190ca3b7457fb4881
tic-top <kirp@umich.edu>
2024-03-27 17:26:18 -0400

    test bash

--- test.sh
+++ test.sh
@@ -7,7 +7,7 @@
     	--benchmark tpcds-1gb-iceberg-load
 
 ./run-benchmark.py \
- --cluster-hostname gl3035.arc-ts.umich.edu \
+ --cluster-hostname gl3076.arc-ts.umich.edu \
     	-i /home/kirp/.ssh/id_ed25519 \
     	--ssh-user kirp \
     	--benchmark-path /myresult/load-1gb-query \
@@ -15,16 +15,16 @@
 		--iterations 1
 
 ./run-benchmark.py \
-	--cluster-hostname gl3035.arc-ts.umich.edu \ 
+	--cluster-hostname gl3076.arc-ts.umich.edu \ 
     --i /home/kirp/.ssh/id_ed25519 \
 	--ssh-user kirp \
 	--source-path "file:///home/kirp/db-scratch/try-1"    
 	--scale 1 
-	--cache-path "hdfs://hadoop1:9000/lhbench-cache-xzw" 
-	--benchmark-path "hdfs://hadoop1:9000/lhbench-tables-xzw/"  
+	--cache-path "hdfs://10.164.8.126:9000/lhbench-cache/" 
+	--benchmark-path "hdfs://10.164.8.126:9000/lhbench-table/"  
 	--db-name "icebergload"  
 	--table-mode cow 
 	--partition-tables true 
 	--benchmark tpcds-custom-ingestion-iceberg    
 	--refresh-count 10  
-	--iterations 3
+	--iterations 1
